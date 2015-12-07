6e3d2e143a0012c064b139b3df0ecc7a774a3d65
Kelli Pando <kpandette@gmail.com>
2015-12-06 21:54:45 -0800

    Added unity project files

---
+++ Assets/Block.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        // Destroy the whole block
+        Destroy(gameObject);
+    }
+}
