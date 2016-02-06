db6162531b97eacecb7436113d53df31ba3a8ccd
Justin Pando <justin.pando@gmail.com>
2016-02-05 18:40:45 -0800

    Added new image assets, added ball escape detection and random phrases

--- Assets/Scripts/Game Logic/Ball.cs
+++ Assets/Scripts/Game Logic/Ball.cs
@@ -7,6 +7,8 @@ public class Ball : MonoBehaviour {
 
 	Vector3 startPos;
 
+    CoroutineManager.Item escapeDetectionRoutine = new CoroutineManager.Item();
+
 	// Use this for initialization
 	void Awake () {
         // Initial Velocity
@@ -18,10 +20,52 @@ public class Ball : MonoBehaviour {
 	}
 
 	void Initialize() {
-		bool right = Random.value < 0.5f;
 
-		rigidbody.velocity = (right == true ? Vector2.right : Vector2.left) * speed.value;
-	}
+
+        string[] randomPhrases = new string[]
+        {
+            "shmex it",
+            "hai",
+            "hey friends",
+            "you're all so attractive",
+            "SHMEX",
+            "shmeeeeeexxxxx",
+            "this game is called Shmexball? Weird.",
+            "Get Ready",
+            "you've made us all so happy",
+            "you are enjoying this game",
+            "do you know something about algebra?",
+            "i hope you slept well!!",
+            "my brother loves this game",
+            "oh, let's get married!!",
+            "i love you",
+            "ni xiang buxiang bofang SHMEX?",
+            "GET FUELED.",
+            "welcome",
+            "this is an innovative experience"
+        };
+
+        DisplayBroadcastManager.DisplayMessage(randomPhrases.PickRandom(), 3f);
+
+        //GetComponent<BoxCollider2D>().enabled = false;
+
+        StartCoroutine(InitializeRoutine());
+    }
+
+    IEnumerator InitializeRoutine()
+    {
+
+
+        yield return new WaitForSeconds(2f);
+
+        bool right = Random.value < 0.5f;
+
+        rigidbody.velocity = (right == true ? Vector2.right : Vector2.left) * speed.value;
+        escapeDetectionRoutine.sequence = EscapeDetectionRoutine();
+
+        //GetComponent<BoxCollider2D>().enabled = true;
+    }
+
 
     float hitFactor(Vector2 ballPos, Vector2 racketPos,
                 float racketHeight)
@@ -67,11 +111,55 @@ public class Ball : MonoBehaviour {
 		speed.value = speed.value * 1.01f;
         ("collided with" + col.gameObject.name).DebugLogJustin();
         //Debug.Log(rigidbody.velocity.magnitude);
+        escapeDetectionRoutine.sequence = EscapeDetectionRoutine();
     }
     
     void Reset() {
-    	transform.position = startPos;
+        StartCoroutine(ResetRoutine());
+    }
+
+    public IEnumerator ResetRoutine()
+    {
+
+        yield return new WaitForSeconds(1f);
+
+        transform.position = startPos;
     	rigidbody.velocity = Vector2.zero;
     	speed.value = 20f;
+
+    }
+
+    public IEnumerator EscapeDetectionRoutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        if (GameController.mode != GameController.Mode.Action) yield break;
+
+        string[] randomPhrases = new string[]
+        {
+            "what did you do with the ball???",
+            "why",
+            "stop knocking the ball out of bounds",
+            "stop it",
+            "hey maybe don't do that",
+            "the shmexmanity",
+            "that's okay! try again!!",
+            "we're still friends, right?",
+            "did you brush your teeth today?",
+            "i do really like butterflies.",
+            "ni hui buhui bofang shmex?! chihan!",
+            "OK OK try again!!",
+            "no crying, keep trying!"
+        };
+
+        DisplayBroadcastManager.DisplayMessage(randomPhrases.PickRandom(), 4f);
+
+        yield return new WaitForSeconds(4f);
+
+        Reset();
+
+        Initialize();
+        
+        
     }
  }
